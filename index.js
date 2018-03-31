d25c69d3bb257f705e015b1375363dc8af6b2a5d
Ryan Wang <samuel9008@gmail.com>
2018-04-01 01:38:20 +0800

    multi-mobile screen connection

---
+++ index.js
@@ -0,0 +1,107 @@
+////////////////////// http server //////////////////////
+var http = require("http");
+var url = require('url');
+var fs = require('fs');
+
+var server = http.createServer(function(request, response) {
+  var path = url.parse(request.url).pathname;
+  console.log('Server on');
+  switch (path) {
+  	case '/':
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write('Hello, World.');
+      response.end();
+      break;
+    default:
+      fs.readFile(__dirname + path, function(error, data) {
+        if (error){
+          response.writeHead(404);
+          response.write("opps this doesn't exist - 404");
+        } else {
+          response.writeHead(200, {"Content-Type": "text/html"});
+          response.write(data, "utf8");
+        }
+        response.end();
+      });
+  }
+});
+server.listen(8000);
+
+////////////////////// serial port //////////////////////
+// DEBUG=serialport:main node index.js
+var SerialPort = require("serialport");
+var port = new SerialPort("/dev/tty.usbmodem1411");
+// port.on('data', function (data) {
+//   console.log('Data:', data);
+// });
+
+//////////////////// socket io //////////////////////
+// DEBUG=socket.io* node myapp
+var io = require("socket.io");
+var sio = io.listen(server);
+
+var client = [];
+
+port.on('data', function (data) { 
+  console.log('收到 ' + data);
+  client.forEach((socket) => {socket.emit('serial', { 'control': data.toString() })});
+});
+
+var p = 0;
+sio.on('connection', function(socket){
+	console.log('socket here');
+	// client in
+  client.push(socket);
+  // anouce index to client 
+  socket.emit('index', { 'index': client.indexOf(socket) });
+  
+  if(client.indexOf(socket) == 0){
+    console.log("sdajkl;fldfjdlj");
+    socket.emit('hasUser', {"hasUser": true});
+  }
+  
+  socket.on('lives', function(data){
+      client.forEach((socket)=>socket.emit('life', data));
+  });
+
+  socket.on('score', function(data){
+      client.forEach((socket)=>socket.emit('score', data));
+  });
+
+  socket.on('restart', function(data){
+      client.forEach((socket)=>socket.emit('restart', data));
+  });
+
+  socket.on('transition', function(data){
+      if(data == "right"){
+          p = client.indexOf(socket) + 1;
+          data = "left";
+      } else if (data == "left") {
+          p = client.indexOf(socket) - 1;
+          data = "right";
+      }
+      p = (p+client.length) % client.length;
+      socket.emit('entry', {"hasUser": false, "entryPoint": data});
+
+      console.log("dsfsd: ", p);
+      client.forEach((socket) => {
+        var hasUser = client.indexOf(socket) === p;
+        console.log(p, " : ",  hasUser, " : ", data);
+        socket.emit('entry', {"hasUser": hasUser, "entryPoint": data});
+      });
+  });
+
+  // if client disconnect
+  socket.on('disconnect', function() {
+    // client out
+    client.splice(client.indexOf(socket), 1);
+    // update index of every client 
+    client.forEach((socket) => {socket.emit('index', { 'index': client.indexOf(socket) })});
+    console.log('disconnected event');
+  });
+
+});
+
+// https://blog.gtwang.org/programming/socket-io-node-js-realtime-app/
+// https://www.npmjs.com/package/socket.io-serialport
+// http://helloraspberrypi.blogspot.tw/2014/03/nodejssocketioserialport-web-app-to.html
---
+++ sketch_mar27a-2.ino
@@ -0,0 +1,102 @@
+const int buttonPin0 = 0;    // the number of the pushbutton pin
+const int buttonPin1 = 1;
+const int buttonPin2 = 2;
+const int buttonPin3 = 3;
+const int buttonPin4 = 4;
+const int led = 5 ;
+
+// Variables will change:
+int a ;
+int ledState = HIGH;         // the current state of the output pin
+int buttonState0;             // the current reading from the input pin
+int buttonState1;             // the current reading from the input pin
+int buttonState2;             // the current reading from the input pin
+int buttonState3;             // the current reading from the input pin
+int buttonState4;             // the current reading from the input pin
+int lastButtonState0 = LOW;   // the previous reading from the input pin
+int lastButtonState1 = LOW;   // the previous reading from the input pin
+int lastButtonState2 = LOW;   // the previous reading from the input pin
+int lastButtonState3 = LOW;   // the previous reading from the input pin
+int lastButtonState4 = LOW;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+int readingled ;
+int b = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin0, INPUT);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(buttonPin3, INPUT);
+  pinMode(buttonPin4, INPUT);
+  pinMode(led , INPUT) ;
+}
+
+void loop() {
+  // read the state of the switch into a local variable:
+  int reading0 = digitalRead(buttonPin0);
+  int reading1 = digitalRead(buttonPin1);
+  int reading2 = digitalRead(buttonPin2);
+  int reading3 = digitalRead(buttonPin3);
+  int reading4 = digitalRead(buttonPin4);
+  int readingled = digitalRead(led) ;
+  if (readingled == 1)
+  { b = 1;
+    }
+  else
+  {
+    b = 0 ;
+    }
+  // If the switch changed, due to noise or pressing:
+  if (reading0 != lastButtonState0 || reading1 != lastButtonState1 || reading2 != lastButtonState2 || reading3 != lastButtonState3 || reading4 != lastButtonState4 ) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading0 != buttonState0) {
+      buttonState0 = reading0;
+
+      if (buttonState0 == HIGH) {
+        a=0;
+      }
+    }
+    if (reading1 != buttonState1) {
+      buttonState1 = reading1;
+
+      if (buttonState1 == HIGH) {
+        a=1;
+      }
+    }
+    if (reading2 != buttonState2) {
+      buttonState2 = reading2;
+
+      if (buttonState2 == HIGH) {
+        a=2;
+      }
+    }
+    if (reading3 != buttonState3) {
+      buttonState3 = reading3;
+
+      if (buttonState3 == HIGH) {
+        a=3;
+      }
+    }
+    if (reading4 != buttonState4) {
+      buttonState4 = reading4;
+
+      if (buttonState4 == HIGH) {
+        a=4;
+      }
+    }
+  }
+
+
+  lastButtonState0 = reading0;
+  lastButtonState1 = reading1;
+  lastButtonState2 = reading2;
+  lastButtonState3 = reading3;
+  lastButtonState4 = reading4;
+  Serial.println(a);
+}
